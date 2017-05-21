342b7bd13264c32d5c98024cf4b6a3fd45684efd
Ramin Sharafkandi <r.sharafkandi@gmail.com>
2017-05-21 20:25:15 +0430

    new shahkar estelam script was added

--- deployment/roles/mnp_monitoring_zabbix/files/get_mnp_shahkar_estelaam_call_count.sh
+++ deployment/roles/mnp_monitoring_zabbix/files/get_mnp_shahkar_estelaam_call_count.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # This script is part of Rightel Portability Monitoring Project at Tookasoft
-# April 2017
+# May 2017
 #
 # Developer: Ramin Sharafkandi
 # Head of Team: Hamed Shakiba
@@ -10,7 +10,6 @@ MYSQL_USERNAME="root"
 MYSQL_PASSWORD=""
 MYSQL_HOSTNAME="localhost"
 MYSQL_SCHEMA="mnp"
-WEB_SERVICE_NAME=""
 DEBUG_MODE="False"
 DEBUG_LOG="/tmp/zabbix_mnp_monitoring.log"
 SCRIPT_NAME=$( basename $0 )
@@ -58,19 +57,10 @@ while [ True ]; do
         MYSQL_SCHEMA=$1
         shift
         ;;
-     "-w" | "--webservice" )
-        shift
-        [ -z "$1" ] && [ $DEBUG_MODE == "True" ] && logger "ERROR! -w/--webservice must be followed by a web service name"
-        [ -z "$1" ] && exit 1
-        WEB_SERVICE_NAME=$( echo "$1" | tr [:upper:] [:lower:] )
-        shift
-        ;;
      "--help" )
         echo "Usage $0 [-u|--username USERNAME] [-p|--password PASSWORD] [-h|--hostname HOSTNAME] [-s|--schema SCHEMANAME]"
-        echo "         -w|--webservice SERVICENAME"
         echo
-        echo "   This script runs a query against mysql database to get fail count of call of the webservice whose name is passed to script"
-        echo "during last 30 minutes."
+        echo "   This script runs a query against mysql database to get fail count of call of the estelaam Shahkar webservice"
         echo
         exit 0
         ;;
@@ -82,20 +72,15 @@ while [ True ]; do
 
 done
 
-if [ -z "$WEB_SERVICE_NAME" ]; then
-  [ $DEBUG_MODE == "True" ] && logger "ERROR: No web-service name was passed to script"
-  exit 1
-fi
-
 start_date=`date --date="-30 minutes" +"%Y-%m-%d %H:%M:%S"`
 end_date=`date +"%Y-%m-%d %H:%M:%S"`
 
 [ $DEBUG_MODE == "True" ] && logger "/usr/bin/mysql -sN -u ${MYSQL_USERNAME} --password=${MYSQL_PASSWORD} -h ${MYSQL_HOSTNAME} ${MYSQL_SCHEMA}"
-[ $DEBUG_MODE == "True" ] && logger "select count(*) from mnp_webservicelog where status = 0 and LOWER(webServiceName) = '${WEB_SERVICE_NAME}' and requestDate between '${start_date}' and '${end_date}'"
+[ $DEBUG_MODE == "True" ] && logger "select count(*) from mnp_shahkarCallLog where shahkarResponseCode <> 2000 and requestDate between '${start_date}' and '${end_date}'"
 
 result=`
 /usr/bin/mysql -sN -u ${MYSQL_USERNAME} --password=${MYSQL_PASSWORD} -h ${MYSQL_HOSTNAME} ${MYSQL_SCHEMA} 2>&1 << EOF1:
-select count(*) from mnp_webservicelog where status = 0 and LOWER(webServiceName) = '${WEB_SERVICE_NAME}' and requestDate between '${start_date}' and '${end_date}';
+select count(*) from mnp_shahkarResponseCode where shahkarResponseCode <> 200 and requestDate between '${start_date}' and '${end_date}';
 EOF1:
 `
 
